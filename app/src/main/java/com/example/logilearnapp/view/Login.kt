c6281a02fe64190be98905beb22e7baeff65c15a
lucia <luciaone34@gmail.com>
2023-12-27 00:06:44 +0100

    creación de vistas Login y Register

---
+++ app/src/main/java/com/example/logilearnapp/view/Login.kt
@@ -0,0 +1,12 @@
+package com.example.logilearnapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.logilearnapp.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
