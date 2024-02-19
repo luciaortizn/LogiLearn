21d1734f79712ccda6cced4039f6fe15bc622f2c
lucia <luciaone34@gmail.com>
2024-02-19 18:51:18 +0100

    creación bd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,5 +96,11 @@ dependencies {
     implementation("com.firebaseui:firebase-ui-auth:7.2.0")
 
 
+     //firebase
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    // Declare the dependency for the Cloud Firestore library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-firestore")
 
 }
